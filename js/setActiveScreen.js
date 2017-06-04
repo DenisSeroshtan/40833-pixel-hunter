135f2ba01cf07a53534078fb8dc9da95b618041e
Roman Milkovic <destabiliseus@gmail.com>
2017-06-04 17:21:49 +0800

    Вернул недостающий div

--- js/setActiveScreen.js
+++ js/setActiveScreen.js
@@ -1,5 +1,5 @@
 export default function setActiveScreen(screen) {
-  const mainScreen = document.querySelector(`.central`);
+  const mainScreen = document.getElementById(`main`);
   mainScreen.innerHTML = ``;
   mainScreen.appendChild(screen);
 }
